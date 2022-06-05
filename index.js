124007344a6d9bb35f14ddd5e58ea0dc0e283813
verysetiabudi <verysetiabudi37@gmail.com>
2022-06-05 20:26:28 +0700

    feat : first commit challenge

--- index.js
+++ index.js
@@ -6,15 +6,26 @@ const names = ["Halo", "Angel", "Nyoman", "Ketut", "Aisyah"];
 // "1. Nama"
 // "2. Nama"
 // ...
-const sorter = null;
+const sorter = (arrayName, fnSort) => {
+  let data = [];
+  let dataName = fnSort(arrayName);
+  for (let counter = 0; counter < dataName.length; counter++) {
+    data.push(`${counter + 1}. ${dataName[counter]}`);
+  }
+  return data;
+};
 
 // TODO: Fungsi untuk mengurutkan array of string secara ascending
 // mengembalikan array yang sudah diurutkan
-const sortAscending = null;
+const sortAscending = (arrayName) => {
+  return arrayName.sort();
+};
 
 // TODO: Fungsi untuk mengurutkan array of string secara descending
 // mengembalikan array yang sudah diurutkan
-const sortDescending = null;
+const sortDescending = (arrayName) => {
+  return arrayName.sort().reverse();
+};
 
 // ! JANGAN DIMODIFIKASI
 (function main() {
